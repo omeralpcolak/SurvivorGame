7b241a504184db335912ad4fb6c246307223a632
Ömer Alp Çolak <omer.alp.colak@gmail.com>
2024-01-12 19:16:43 +0300

    Update MainMenuManager.cs

--- SurvivorGame/Assets/Scripts/GameManager/MainMenuManager.cs
+++ SurvivorGame/Assets/Scripts/GameManager/MainMenuManager.cs
@@ -7,10 +7,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public enum MenuType
+    {
+        MAINMENU,
+        MAPMENU,
+        PLAYERSELECTIONMENU
+    }
+
+    private GameObject currentMenu;
+
+    public GameObject mainMenu;
+    public GameObject mapMenu;
+    public GameObject playerSelectionMenu;
+
+    public GameSelections gameSelections;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentMenu = mainMenu;
     }
 
     // Update is called once per frame
@@ -22,5 +36,38 @@ public class MainMenuManager : MonoBehaviour
     public void Test()
     {
         Debug.Log("clicked");
+    }
+
+    public void StartTheGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void BackToMainMenu()
+    {
+        ActivateMenu(MenuType.MAINMENU);
+    }
+
+    public void ActivateMenu(MenuType menuType)
+    {
+        currentMenu.GetComponent<CanvasGroup>().DOFade(0, 1f);
+        currentMenu.SetActive(false);
+
+        switch (menuType)
+        {
+            case MenuType.MAINMENU:
+                currentMenu = mainMenu;
+                break;
+            case MenuType.MAPMENU:
+                currentMenu = mapMenu;
+                break;
+            case MenuType.PLAYERSELECTIONMENU:
+                currentMenu = playerSelectionMenu;
+                break;
+
+        }
+
+        currentMenu.SetActive(true);
+        currentMenu.GetComponent<CanvasGroup>().DOFade(1, 1f);
     }
 }
