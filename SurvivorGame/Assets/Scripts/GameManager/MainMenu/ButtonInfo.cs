f564c03bb4d9fc6d0d6a4d48990daaea5b667d07
Ömer Alp Çolak <omer.alp.colak@gmail.com>
2024-01-16 13:10:46 +0300

    ButtonInfo.cs

--- SurvivorGame/Assets/Scripts/GameManager/MainMenu/ButtonInfo.cs
+++ SurvivorGame/Assets/Scripts/GameManager/MainMenu/ButtonInfo.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class ButtonInfo : MonoBehaviour
 {
     public GameSelections gameSelections;
     public GameObject holdingObject; //map or character.
     public bool isItMap;
+    public Sprite icon;
+    public string text;
+    private Button button;
+    private TMP_Text textComponent;
+
+    private void OnEnable()
+    {
+        button = GetComponent<Button>();
+        textComponent = GetComponentInChildren<TMP_Text>();
+        button.GetComponent<Image>().sprite = icon;
+        textComponent.text = text;
+        
+    }
 
     public void SelectGameobject()
     {
