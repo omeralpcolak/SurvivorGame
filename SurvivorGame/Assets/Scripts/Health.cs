df5fc110262e97f12ebac27e67f21d4533ee5957
Ömer Alp Çolak <omer.alp.colak@gmail.com>
2024-02-11 12:27:54 +0300

    Update on Health.cs

--- SurvivorGame/Assets/Scripts/Health.cs
+++ SurvivorGame/Assets/Scripts/Health.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+public enum EntityType
+{
+    Player,
+    Enemy
+}
+
 public class Health : MonoBehaviour
 {
     public int health;
@@ -11,9 +17,11 @@ public class Health : MonoBehaviour
     public bool isObjectEnemy;
     public GameObject xp;
     public GameObject coin;
-
+    public EntityType entityType;
     private int currentHealth;
     [SerializeField] private Healthbar healthbar;
+
+   
 
     private void Start()
     {
@@ -27,13 +35,21 @@ public class Health : MonoBehaviour
         healthbar.UpdateHealthBar(health, currentHealth);
         Instantiate(hitEffect, transform.position, Quaternion.identity,transform);
         CameraShake.instance.Shake(0.4f, 2f);
+
         if (currentHealth<= 0)
         {
-            if (isObjectEnemy)
+
+            switch (entityType)
             {
-                Instantiate(xp, transform.position, Quaternion.identity);
-                Instantiate(coin, transform.position, Quaternion.identity);
+                case EntityType.Player:
+                    //player taking damage
+                    break;
+                case EntityType.Enemy:
+                    Instantiate(xp, transform.position, Quaternion.identity);
+                    Instantiate(coin, transform.position, Quaternion.identity);
+                    break;
             }
+
             Instantiate(deathEffect, transform.position, Quaternion.identity);
             Destroy(gameObject);
         }
