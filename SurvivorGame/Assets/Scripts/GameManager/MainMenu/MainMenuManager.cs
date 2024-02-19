bee0b0aa228835b85b715aea69e6d63d35a4b9d4
Ömer Alp Çolak <omer.alp.colak@gmail.com>
2024-02-19 18:09:50 +0300

    Update MainMenuManager.cs

--- SurvivorGame/Assets/Scripts/GameManager/MainMenu/MainMenuManager.cs
+++ SurvivorGame/Assets/Scripts/GameManager/MainMenu/MainMenuManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -15,10 +16,11 @@ public class MainMenuManager : MonoBehaviour
     }
     public static MainMenuManager instance;
     private GameObject currentMenu;
-
+    public  CinemachineVirtualCamera cam;
     public GameObject mainMenu;
     public GameObject mapMenu;
     public GameObject playerSelectionMenu;
+
     
 
     public GameSelections gameSelections;
@@ -27,6 +29,7 @@ public class MainMenuManager : MonoBehaviour
     {
         instance = this;
         currentMenu = mainMenu;
+        
     }
     
     public void StartTheGameScene()
@@ -40,20 +43,24 @@ public class MainMenuManager : MonoBehaviour
 
     public void ActivatePlayerSelectionMenu()
     {
+        CamMove(new Vector3(0,1,-7));
         ActivateMenu(MenuType.PLAYERSELECTIONMENU);
     }
 
     public void ActivateMapMenu()
     {
+        CamMove(new Vector3(0,10,-15));
         ActivateMenu(MenuType.MAPMENU);
     }
     public void BackToMainMenu()
     {
+        CamMove(new Vector3(0, 1, -15));
         ActivateMenu(MenuType.MAINMENU);
     }
 
     public void ActivateMenu(MenuType menuType)
     {
+        
         currentMenu.GetComponent<CanvasGroup>().DOFade(0, 1f);
         currentMenu.SetActive(false);
 
@@ -70,8 +77,13 @@ public class MainMenuManager : MonoBehaviour
                 break;
 
         }
-
+        
         currentMenu.SetActive(true);
         currentMenu.GetComponent<CanvasGroup>().DOFade(1, 1f);
+    }
+
+    private void CamMove(Vector3 pos)
+    {
+        cam.transform.DOMove(pos, 1f);
     }
 }
